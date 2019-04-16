078332e5f708f67ea710f9d782fb5494b8e32895
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2019-04-16 22:02:15 +0200

    C3D2 redesign

--- js/data.js
+++ js/data.js
@@ -1,44 +1,46 @@
 
-var headerText = "Bingo";
-
-var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
-
+var headerText = "C3D2 Bullsh*t-Bingo";
+var footerText = "<a href='https://github.com/c3d2/c3d2-bullshit-bingo/' target='_blank'>Code available on github</a>";
 var winText = "Winner";
-
 var clickSnd = new Audio("audio/click.mp3");
-
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
+        {"square": "in Rust neuschreiben"},
+        {"square": "Haskell"},
+        {"square": "Semantic Web"},
+        {"square": "$service ist down"},
+        {"square": "Plenum"},
+        {"square": "Hat das IPv6?"},
+        {"square": "Bitcoin/Cryptocurrency/Blockchain"},
+        {"square": "Keller"},
+        {"square": "Miau"},
+        {"square": "`make clean`"},
+        {"square": "[$person schläft]"},
+        {"square": "Poettering-Software"},
+        {"square": "steht im Wiki"},
+        {"square": "Kann man das (noch) essen?"},
+        {"square": "NixOS"},
+        {"square": "Wortwitz mit \"Nix\""},
+        {"square": "Geschirrspüler ist voll/fertig"},
+        {"square": "Kaffee"},
+        {"square": "Chaospost/Packstation"},
+        {"square": "was mit LEDs"},
+        {"square": "Penta..."},
+        {"square": "Matemat"},
+        {"square": "Eis ist alle"},
+        {"square": "Jemand Bock auf Nudeln?"},
+        {"square": "Planet Döner"},
+        {"square": "Edeka"},
+        {"square": "OpenRA"},
+        {"square": "BSD(ienstag)"},
+        {"square": "Starkduftkabine"},
+        {"square": "server1/3/4/5/6"},
+        {"square": "Wo ist mein $gegenstand hin?"},
+        {"square": "Gar nicht so einfach"},
+        {"square": "müsste man mal"},
+        {"square": "kommt drauf an"},
+        {"square": "...bert"},
+        {"square": "ping / pong"},
     ]
 };
